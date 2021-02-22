a43ef6a9f3a853d9c8ef5cf035e239eb3cffa2c3
lq <max.li@madv.com>
2021-02-22 16:15:42 +0800

    websocket

--- src/main.js
+++ src/main.js
@@ -23,28 +23,8 @@ import * as filters from './filters' // global filters
 import permission from '@/directive/permission/index.js' // 权限判断指令
 import elDragDialog from '@/directive/el-drag-dialog'
 
-import VueHighlightJS from 'vue-highlightjs'
-import 'highlight.js/styles/atom-one-dark.css'
-import AmapVue from '@amap/amap-vue'
-
-AmapVue.config.version = '2.0' // 默认2.0，这里可以不修改
-AmapVue.config.key = 'd3a2d814e3000f9136d7a43084cf9524'
-AmapVue.config.plugins = [
-  'AMap.ToolBar',
-  'AMap.MoveAnimation'
-  // 在此配置你需要预加载的插件，如果不配置，在使用到的时候会自动异步加载
-]
-Vue.use(AmapVue)
-
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
 Vue.config.productionTip = false
-import Form from 'ant-design-vue/lib/form'
-Vue.component(Form)
 
-Vue.use(Antd)
-
-Vue.use(VueHighlightJS)
 Vue.use(Element, {
   size: Cookies.get('size') || 'medium', // set element-ui default size
   i18n: (key, value) => i18n.t(key, value)
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <div style="text-align:center;font-size:30px;">
-    管理后台
-  </div>
+  <div style="text-align: center; font-size: 30px">管理后台</div>
 </template>
+
 <script>
+import { connectionSocket } from "../../utils/websocket";
+
 export default {
-  name: 'Dashboard'
-}
+  name: "Dashboard",
+  created() {
+    connectionSocket()
+  },
+};
 </script>
